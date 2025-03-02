6490b452c35f83ff8165025656f9bfa74b0a0293
Jennifer McGrath <jen.mcgrath01@gmail.com>
2025-03-02 14:49:13 -0700

    render files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,16 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/staff_planner/',
+  base: '/',
+  server: {
+    port: 5173,
+    open: true
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'staff_planner/assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'staff_planner/assets/[name].[hash].js',
-        chunkFileNames: 'staff_planner/assets/[name].[hash].js',
-        assetFileNames: 'staff_planner/assets/[name].[hash].[ext]'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
